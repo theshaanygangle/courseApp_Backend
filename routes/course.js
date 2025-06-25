56672c43fa2ac84fa4539a3b184e073a0f79f9fc
Shaany Gangle <shaanygangle21@gmail.com>
2025-06-25 15:57:09 +0530

    Added course Route

--- routes/course.js
+++ routes/course.js
@@ -3,13 +3,28 @@ const {courseModel} = require ("../db.js");
 
 const courseRouter = Router();
       
-courseRouter.get("/purchases", function (req,res){
-      
+courseRouter.post("/purchase", userMiddleware, async function(req, res) {
+    const userId = req.userId;
+    const courseId = req.body.courseId;
+
+    // should check that the user has actually paid the price
+    await purchaseModel.create({
+        userId,
+        courseId
+    })
+
+    res.json({
+        message: "You have successfully bought the course"
+    })
 })
 
-courseRouter.get("/allCourses", function (req,res){
+courseRouter.get("/preview", async function(req, res) {
+    
+    const courses = await courseModel.find({});
 
-      
+    res.json({
+        courses
+    })
 })
 
 
